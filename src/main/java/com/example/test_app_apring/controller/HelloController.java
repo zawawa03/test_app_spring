d56b06a778d934e8a670435aa5ea698bfb9a1e91
tatsuya03 <yszw7845hg@gmail.com>
2026-04-01 18:01:30 +0900

    second_commit

--- src/main/java/com/example/test_app_apring/controller/HelloController.java
+++ src/main/java/com/example/test_app_apring/controller/HelloController.java
@@ -1,12 +1,21 @@
 package com.example.test_app_apring.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String getHello(){
         return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String postRequest(@RequestParam("name") String str, Model model) {
+        model.addAttribute("sample", str);
+        return "response";
     }
 }
