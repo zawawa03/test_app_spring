21578dc058dfb7fdf83a7af5e8ffdd02107a2700
tatsuya03 <yszw7845hg@gmail.com>
2026-04-07 19:07:17 +0900

    layout

--- src/main/java/com/example/test_app_apring/controller/LoginController.java
+++ src/main/java/com/example/test_app_apring/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.test_app_apring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -9,4 +10,7 @@ public class LoginController {
     public String getLogin(){
         return "login/login";
     }
+
+    @PostMapping("/login")
+    public String postLogin() {return "redirect:/user/list"; }
 }
