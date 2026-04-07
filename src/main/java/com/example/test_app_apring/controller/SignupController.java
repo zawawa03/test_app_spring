4667563fbf1a720be7a45a9dd160fb3d595709e1
tatsuya03 <yszw7845hg@gmail.com>
2026-04-07 11:08:18 +0900

    error_message

--- src/main/java/com/example/test_app_apring/controller/SignupController.java
+++ src/main/java/com/example/test_app_apring/controller/SignupController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import com.example.test_app_apring.sevice.UserApplicationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,7 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignup(Model model, Locale locale, @ModelAttribute("signupForm") SignupForm form, BindingResult bindingResult) {
+    public String postSignup(Model model, Locale locale, @ModelAttribute("signupForm") @Validated SignupForm form, BindingResult bindingResult) {
         if(bindingResult.hasErrors()){
             return getSignup(model, locale, form);
         }
--- src/main/java/com/example/test_app_apring/form/SignupForm.java
+++ src/main/java/com/example/test_app_apring/form/SignupForm.java
@@ -1,6 +1,8 @@
 package com.example.test_app_apring.form;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
@@ -8,13 +10,28 @@ import java.util.Date;
 @Data
 
 public class SignupForm {
+
+    @NotBlank
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min = 4, max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
+
+    @NotBlank
     private String userName;
+
+    @Min(20)
+    @Max(100)
     private Integer age;
+
+    @NotNull
     private Integer gender;
 
     @DateTimeFormat(pattern="yyyy/MM/dd")
+    @NotNull
     private Date birthday;
 }
 
